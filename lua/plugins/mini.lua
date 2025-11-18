79fa7b0860311b5d260a2fbe8426804c1507aa07
Anwaya Rai <rai.anwaya@gmail.com>
2025-11-18 17:54:21 +0530

    Moved statusline to top and added hipatterns plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,18 +5,43 @@ local M = {
 M.config = function()
   require('mini.ai').setup { n_lines = 500 }
   require('mini.surround').setup()
-  local statusline = require 'mini.statusline'
 
+  local statusline = require 'mini.statusline'
   statusline.section_mode = function()
     return ''
   end
-
   statusline.section_fileinfo = function()
     return vim.bo.filetype
   end
-
   statusline.setup {
     set_vim_settings = false,
+  }
+  -- Move statusline to the top
+  vim.o.winbar = '%{%v:lua.MiniStatusline.active()%}'
+  vim.o.laststatus = 0
+
+  local hipatterns = require 'mini.hipatterns'
+  hipatterns.setup {
+    highlighters = {
+      fixme = {
+        pattern = '%f[%w]()FIXME()%f[%W]',
+        group = 'MiniHipatternsFixme',
+      },
+      hack = {
+        pattern = '%f[%w]()HACK()%f[%W]',
+        group = 'MiniHipatternsHack',
+      },
+      todo = {
+        pattern = '%f[%w]()TODO()%f[%W]',
+        group = 'MiniHipatternsTodo',
+      },
+      note = {
+        pattern = '%f[%w]()NOTE()%f[%W]',
+        group = 'MiniHipatternsNote',
+      },
+
+      hex_color = hipatterns.gen_highlighter.hex_color(),
+    },
   }
 end
 
